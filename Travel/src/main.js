c468820935040e8845f626e3361e3ec03d0f7e39
LiJiaWei <525469647@qq.com>
2018-09-18 09:58:45 +0800

    project init

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import fastClick from 'fastclick'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: { App }, //相当于 {App（局部组件）:App} ES6写法
   template: '<App/>'
 })
+
+//路由就是根据网址的不同，
+// 返回不同的内容给用户
---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="home">
+       My home
+    </div>
+  </div>
+</template>
+
+<script>
+  //export default 指的是导出，意思是定一个一个组件，然后把组件导出出去
+export default{
+    name:'Home'
+  }
+
+</script>
+
+

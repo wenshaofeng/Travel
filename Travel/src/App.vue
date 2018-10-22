37604d96e147073184f08d2c2e2a00fdcb957b85
LiJiaWei <525469647@qq.com>
2018-10-22 22:41:20 +0800

    Home scroll

--- Travel/src/App.vue
+++ Travel/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-
+  <div id="app" style="height: 100%">
     <router-view/>
   </div>
 </template>
@@ -10,7 +9,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-
-</style>
--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,23 +1,25 @@
 <template>
-  <div>
-
-       <home-header :city="city"></home-header>
-       <home-swiper :list="swiperList"></home-swiper>
-        <icons :iconList="iconList"></icons>
-        <home-recommend :recommendList="recommendList">
-        </home-recommend>
-        <weekend :list="weekendList"></weekend>
+  <div class="list" ref="wrapper">
+    <div>
+      <home-header :city="city"></home-header>
+      <home-swiper :list="swiperList"></home-swiper>
+      <icons :iconList="iconList"></icons>
+      <home-recommend :recommendList="recommendList">
+      </home-recommend>
+      <weekend :list="weekendList"></weekend>
+    </div>
   </div>
 </template>
 
 <script>
   //export default 指的是导出，意思是定一个一个组件，然后把组件导出出去
-import HomeHeader from "./components/HomeHeader.vue";
-import HomeSwiper from "./components/Swiper.vue"
-import icons from "./components/icons.vue"
-import HomeRecommend from "./components/HomeRecommand.vue"
-import Weekend from "./components/Weekend.vue"
-import axios from "axios"
+  import HomeHeader from "./components/HomeHeader.vue";
+  import HomeSwiper from "./components/Swiper.vue"
+  import icons from "./components/icons.vue"
+  import HomeRecommend from "./components/HomeRecommand.vue"
+  import Weekend from "./components/Weekend.vue"
+  import axios from "axios"
+  import BScroll from 'better-scroll'
 export default{
     name:'Home',
     data (){
@@ -51,14 +53,19 @@ export default{
                 this.recommendList = data.recommendList
                 this.weekendList = data.weekendList
             }
-        }
+        },
+
     },
         mounted () {
             this.getHomeInfo()
+          const scroll = new BScroll(this.$refs.wrapper)
         }//页面挂载好了以后执行
 
   }
 
 </script>
 
-
+<style lang="stylus"  rel='stylesheet/stylus' scoped>
+  .list
+    height 100%
+</style>
